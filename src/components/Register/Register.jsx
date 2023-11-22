acc37df0e5d602bafe0062058ae40c877861923b
Arafat002 <mhyzdamilola@gmail.com>
2023-11-22 09:21:52 +0100

    register

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,36 @@
+// src/components/RegistrationPage/RegistrationPage.js
+import React, { useState } from "react";
+import CountryInfo from "./country";
+import AccountInfo from "./details";
+
+const RegistrationPage = () => {
+  const [page, setPage] = useState(1); // 1 for PersonalInfo, 2 for AccountInfo
+
+  const handleNext = () => {
+    setPage(2);
+  };
+
+  const handlePrev = () => {
+    setPage(1);
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={handlePrev} disabled={page === 1}>
+          Country Information
+        </button>
+        <button onClick={handleNext} disabled={page === 2}>
+          Account Information
+        </button>
+      </div>
+      {page === 1 ? (
+        <CountryInfo onNext={handleNext} />
+      ) : (
+        <AccountInfo onPrev={handlePrev} />
+      )}
+    </div>
+  );
+};
+
+export default RegistrationPage;
