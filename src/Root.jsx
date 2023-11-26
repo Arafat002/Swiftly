1e4f67586daed02dfef6e4247e179e841cfeab7f
David <td.bolaji@gmail.com>
2023-11-26 23:55:07 +0100

    login page styling

--- src/Root.jsx
+++ src/Root.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login/Login"; // Import your Login component
 import Register from "./components/Register/Register";
-
+import Dashboard from "./components/Dashboard/dashboard";
+import LandingPage from "./components/Landing";
 function Root() {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+      <Routes>
         <Route path="/login" element={<Login />} />
       </Routes>
       <Routes>
-        <Route path="/register" element={<Register />} />
+        <Route path="/register/*" element={<Register />} />
+      </Routes>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );
